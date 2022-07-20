667409709e28c86fc3f8e43a70707d6e8a530c16
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-20 12:44:58 -0500

    :bookmark: 2.0.0

--- scripts/node.ts
+++ scripts/node.ts
@@ -1,9 +1,17 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.17/node.ts";
 
-buildPackage({
-  repoName: "@ultirequiem/sum",
-  description: "Sum utilities.",
-  homepage: "https://sum.js.org",
-  keywords: ["math", "sum"],
-  version: "2.0.0",
-});
+buildPackage(
+  {
+    repoName: "@ultirequiem/sum",
+    description: "Sum utilities.",
+    homepage: "https://sum.js.org",
+    keywords: ["math", "sum"],
+    version: "2.0.0",
+  },
+  {
+    entryPoints: ["./mod.ts", { name: "sum", path: "./sum.ts", kind: "bin" }],
+    typeCheck: false,
+    supportCJS: false,
+    shims: { deno: true, prompts: true },
+  },
+);
